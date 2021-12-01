f04a94affc6b5d86b01d47dfd63e9e506bb7669b
a13xand9r <a13x.student@yandex.ru>
2021-12-01 17:20:45 +0300

    request prod and dev

--- src/api/words.ts
+++ src/api/words.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const getWords = async () => {
-    const { data } = await axios.get<string[]>('http://localhost:5000/words')
+    const { data } = await axios.get<string[]>(process.env.NODE_ENV === 'development' ? 'http://localhost:5000/words' : 'https://alias-server-sber.vercel.app/words')
     return data
 }
